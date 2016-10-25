35b2d33331b86727d4c5aa364c43551cabaf6b75
Adam Felix <adam.tyler.felix@gmail.com>
2016-10-25 13:35:23 -0400

    Initial commit for generator and solver files.

---
+++ app/generator.py
@@ -0,0 +1,5 @@
+# file:     generator.py
+# author:   Adam Felix
+# help:     Sudoku Solving with C (Giuluo Zambon)
+#           and
+#           Programming Sudoku (Wei-Meng Lee)
---
+++ app/solver.py
@@ -0,0 +1,5 @@
+# file:     solver.py
+# author:   Adam Felix
+# help:     Sudoku Solving with C (Giuluo Zambon)
+#           and
+#           Programming Sudoku (Wei-Meng Lee)
