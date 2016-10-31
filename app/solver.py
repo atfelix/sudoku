023aba0f0c18bd953114bfb843cee5ae0dae3adb
Adam Felix <adam.tyler.felix@gmail.com>
2016-10-31 13:19:06 -0400

    Implemented solve and backtracking algorithms.  solve is a skeleton now since the other algorithms have not yet been implemented.

--- app/solver.py
+++ app/solver.py
@@ -5,7 +5,7 @@
 #           Programming Sudoku (Wei-Meng Lee)
 
 
-from sudokugame import SudokuGame
+from sudokugame import SudokuError, SudokuGame
 
 
 
@@ -13,6 +13,9 @@ class SudokuSolver(object):
 
     def __init__(self, boardfile):
         self.game = SudokuGame(boardfile)
+        self.__strategies = [[], [], [], []]
+        self.__strats_used = []
+        self.solve()
 
 
     def __str__(self):
@@ -78,10 +81,40 @@ class SudokuSolver(object):
         return not self.__is_valid()
 
 
-    def __is_valid(self):
-        for i in range(9):
-            for j in range(9):
-                if sum(self.game.entries[i][j]) == 0:
+    def __is_valid(self, char=-1, row=-1, col=-1):
+
+        if char == row == col == -1:
+
+            for i in range(9):
+                for j in range(9):
+                    if sum(self.game.entries[i][j]) == 0:
+                        return False
+
+            return True
+
+        else:
+            if self.__is_valid_row(char=char, row=row):
+                if self.__is_valid_col(char=char, col=col):
+                    if self.__is_valid_box(char=char, row=row, col=col):
+                        return True
+
+            return False
+
+
+    def __is_valid_row(self, char, row):
+        return all(self.game.puzzle[row][i] != char for i in range(9))
+
+
+    def __is_valid_col(self, char, col):
+        return all(self.game.puzzle[i][col] != char for i in range(9))
+
+
+    def __is_valid_box(self, char, row, col):
+        x, y = row // 3 * 3, col // 3 * 3
+
+        for i in range(3):
+            for j in range(3):
+                if self.game.puzzle[x + i][y + j] == char:
                     return False
 
         return True
@@ -93,7 +126,7 @@ class SudokuSolver(object):
 
 
     def __keep_going(self):
-        return self.__count_solved() < 81 and self.is_valid()
+        return self.__count_solved() < 81 and self.__is_valid()
 
 
     def __count_candidates(self):
@@ -107,15 +140,78 @@ class SudokuSolver(object):
 
 
     def solve(self):
-        pass
+        n_candidates = self.__count_candidates()
+        n_old_candidates = n_candidates + 1
+
+        while self.__keep_going() and n_candidates < n_old_candidates:
+            n_old_candidates = n_candidates
+
+            if self.__keep_going() and not self.__execute_strategies(0):
+                if self.__keep_going() and not self.__execute_strategies(1):
+                    if self.__keep_going() and not self.__execute_strategies(2):
+                        self.__execute_strategies(3)
+
+            n_candidates = self.__count_candidates()
+
+        if self.__keep_going():
+            self.__backtrack()
+
+
+    def __execute_strategies(self, level):
+        if level > 3:
+            raise SudokuError
+
+        n_initial_candidates = self.__count_candidates()
+
+        i = 0
+
+        while i < len(self.__strategies[level]) and self.__keep_going():
+            self.__strategies[level][i](self.game.entries)
+
+            if self.__count_candidates() < n_initial_candidates:
+                self.__strats_used.append(10 * level + k)
+                return True
+
+        return False
+
+
+    def __backtrack(self):
+        _unassigned, row, col = self.__unassigned()
+
+        if not _unassigned:
+            return True
+
+        for x in range(9):
+
+            if self.__is_valid(char=x+1, row=row, col=col):
+                self.game.puzzle[row][col] = x + 1
+
+                if self.__backtrack():
+                    return True
+
+                self.game.puzzle[row][col] = 0
+
+        return False
+
+
+    def backtrack(self):
+        self.__backtrack()
+
+
+    def __unassigned(self):
+        for i in range(9):
+            for j in range(9):
+                if self.game.puzzle[i][j] == 0:
+                    return True, i, j
+
+        return False, -1, -1
 
 
 
 def main():
     solver = SudokuSolver(open('n00b.sudoku', 'r'))
     print(solver)
-    print('=' * 100)
-    solver.cleanup()
+    solver = SudokuSolver(open('l33t.sudoku', 'r'))
     print(solver)
 
 
